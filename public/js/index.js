53645abd488fa21287b2df6e615d294c3ef8b717
Jacob Zamano <zamano.jacob@gmail.com>
2019-07-22 10:20:50 -0700

    adding so it can show events!

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,31 @@
-$("#eventTarget").text("hi")
+var htmlString ="";
+
+
+$.ajax({
+    url: "/api/events",
+    method: "GET"
+  }).then(function(res){
+
+      for(var i = 0; i < res.length; i++){
+          var date = res[i].date.substring(0,res[i].date.indexOf("T"))
+          var guestCheck;
+          if(res[i].guest === null){
+              guestCheck =0;
+          }
+          htmlString += `    <div class="col col-1-4">
+          <div class="card">
+            <div class="card_title">
+              <h1>`+res[i].title+`</h1>
+              <hr>
+            </div>
+            <div class="card_content">
+              <hr>
+              <p>Meal Course:`+res[i].bio+`</p>
+              <p>guest limit:`+guestCheck+`/`+res[i].limitGuest+`</p>
+              <p>date:`+date+`</p>
+            </div>
+          </div>
+        </div>`
+      }
+      $("#eventTarget").html(htmlString)
+  })
